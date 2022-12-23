c8735262711f442e90864f7fa8ad113e912e278e
Mirazar Mammdov <mirazarmammadov@Mirazars-MacBook-Pro.local>
2022-12-23 13:53:03 +0530

    Initial Commit

---
+++ Foods Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foods Application
+//
+//  Created by JdeviO on 23/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
